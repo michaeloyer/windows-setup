6f654b9c980fc34af4e422fd9a0eba86cf5ed598
Michael Oyer <code@michaeloyer.com>
2020-05-11 00:01:52 -0400

    Separate Admin and Setup Scripts. Update Readme

--- README.md
+++ README.md
@@ -5,7 +5,14 @@ Includes edits to Powershell, Git, and User Registry Settings.
 
 ## Installation
 
-Run these scripts in the following order
-1. **install_as_admin.ps1** should be run as Administrator
-2. **install_as_user.ps1** 
+Run the setup script first
+
+* **install_as_admin_setup.ps1** (should be run as Administrator)
+
+After that you can run the scripts below to run all scripts in the **/Setup** directory for convenience.
+
+* **install_as_admin.ps1** should be run as Administrator
+* **install_as_user.ps1** 
+
+If you would prefer to only install a subset of the scripts in this repository they can be found in the **/Setup** directory
 
---
+++ install_as_admin_setup.ps1
@@ -0,0 +1,35 @@
+$ModulesDirectory = Join-Path $PSScriptRoot Modules
+
+Import-Module $ModulesDirectory\InstallSetupModule
+
+Write-Host "Installing " -ForegroundColor Yellow -NoNewLine
+Write-Host "PowerShellGet" -ForegroundColor Green
+Install-Module PowerShellGet -Force
+
+if (Test-Command git) {
+    #Allowing Prerelease based on Posh-Git Repo Recommendation. Should remove after 1.0.0 is fully released
+    Install-Module Posh-Git -Force -AllowPrerelease
+}
+else {
+    Write-Host 'git not found in Path, skipping install of Posh-Git'
+}
+
+If((Get-Service ssh-agent) -eq $null) {
+	Write-Host "Installing SSH Client" -ForegroundColor Yellow
+	Add-WindowsCapability -Online -Name OpenSSH.Client* | Format-Table -Property Online,RestartNeeded
+	Write-Host "Installed" -ForegroundColor Blue
+}
+
+Write-Host "Starting SSH Agent Service" -ForegroundColor Yellow
+Get-Service ssh-agent | 
+	Set-Service -StartupType Automatic | 
+	Start-Service
+
+Get-Service ssh-agent | Format-Table
+
+if (Test-Command ssh) {
+	Write-Host "ssh" -ForegroundColor Green -NoNewLine
+	Write-Host " available" -ForegroundColor Yellow
+}
+
+Pause
