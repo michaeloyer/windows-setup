9095f979a8eea2628df8bd0ee27274f9856b2fb2
Michael Oyer <code@michaeloyer.com>
2020-07-19 15:00:49 -0400

    Add GitToSsh Function, Add Git Config Properties

--- Setup/Git/git_global_config_setup.ps1
+++ Setup/Git/git_global_config_setup.ps1
@@ -1,12 +1,19 @@
 Import-Module $PSScriptRoot\GitSetupModule -Force
 
-Add-GitGlobalConfig -Property core.editor -Value vim
-Add-GitGlobalConfig -Property color.ui -Value true
-Add-GitGlobalConfig -Property color.status.changed -Value "magenta bold"
-Add-GitGlobalConfig -Property color.status.untracked -Value "magenta bold"
-Add-GitGlobalConfig -Property color.status.added -Value "green bold"
-Add-GitGlobalConfig -Property color.branch.current -Value "yellow bold reverse"
-Add-GitGlobalConfig -Property color.branch.local -Value "yellow bold"
-Add-GitGlobalConfig -Property color.branch.remote -Value "green bold"
+Add-GitGlobalConfig -Property 'core.editor' -Value 'vim'
 
-Add-GitGlobalConfig -Property format.pretty -Value "%C(auto)%h%d %s %C(bold yellow)(%C(bold magenta)%cN%Cblue %cD %C(bold green)%cr%C(bold yellow))"
+Add-GitGlobalConfig -Property 'color.ui' -Value 'true'
+Add-GitGlobalConfig -Property 'color.status.changed' -Value "magenta bold"
+Add-GitGlobalConfig -Property 'color.status.untracked' -Value "magenta bold"
+Add-GitGlobalConfig -Property 'color.status.added' -Value "green bold"
+Add-GitGlobalConfig -Property 'color.branch.current' -Value "yellow bold reverse"
+Add-GitGlobalConfig -Property 'color.branch.local' -Value "yellow bold"
+Add-GitGlobalConfig -Property 'color.branch.remote' -Value "green bold"
+
+Add-GitGlobalConfig -Property 'format.pretty' -Value "%C(auto)%h%d %s %C(bold yellow)(%C(bold magenta)%cN%Cblue %cD %C(bold green)%cr%C(bold yellow))"
+
+Add-GitGlobalConfig -Property 'pull.rebase' -Value true
+Add-GitGlobalConfig -Property 'rebase.autoStash' -Value true
+
+Add-GitGlobalConfig -Property 'alias.c' -Value "commit -m"
+Add-GitGlobalConfig -Property 'alias.ac' -Value "!git add . && git commit -m"
--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/Functions.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/Functions.ps1
@@ -5,12 +5,25 @@ function vi ($File, [switch]$Verbose) {
 }
 
 function ex ($Dir){
-    if ($Dir -eq $null) { explorer . }
+    if ($null -eq $Dir) { explorer . }
     else { explorer $Dir }
 }
 
 Set-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
-Function cddev {
-	cd $Env:DEV
+function cddev {
+	Set-Location $Env:DEV
+}
+
+function GitToSsh {
+	[CmdletBinding()]
+	param (
+		[ValidateScript({$_.Scheme -eq [Uri]::UriSchemeHttps }, ErrorMessage = 'Must be an HTTPS Uri')]
+		[Uri]$uri
+	) 
+
+	switch ($uri.Host) {
+		'github.com' { return "git@$($uri.Host):$($uri.AbsolutePath.Substring(1))" }
+		Default { throw "Unsupported git host: $($uri.Host)"}
+	}
 }
