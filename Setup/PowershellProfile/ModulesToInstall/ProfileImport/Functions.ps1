b0f944830a362d3ac3c8a0763407171abb1dfba8
Michael Oyer <michael.oyer@ravenind.com>
2020-08-03 23:03:51 -0400

    potential good powershell functions

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/Functions.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/Functions.ps1
@@ -11,6 +11,73 @@ function ex ($Dir){
 
 Set-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
-Function cddev {
+function cddev {
 	cd $Env:DEV
 }
+
+function Verb-Noun {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	begin {
+		
+	}
+	
+	process {
+		
+	}
+	
+	end {
+		
+	}
+}
+
+function Get-WhichFiles ($Path, [switch]$First) {
+	if ($First) {
+		where.exe $Path
+	}
+	else {
+		where.exe $Path | Select-Object -First 1
+	}
+}
+
+Set-Alias which Get-WhichFiles
+
+function Get-FirstItems {
+	param (
+		[Parameter(ValueFromPipeline=$true)]
+		[System.Object]$item,
+        [Parameter(Position=1)]
+        [ValidateRange(1, [int]::MaxValue)]
+		[int]$Count = 1
+	)
+
+	begin { 
+		$index = 0
+	}
+
+	process {
+        if ($index -ge $Count) {
+            return
+        }
+
+		$item
+        $index++
+	}
+}
+
+Set-Alias first Get-FirstItems
+
+function Get-LastItems ([parameter(ValueFromPipeline)]$items, [int]$Count = 1) {
+	$items | Select-Object -Last $Count
+}
+
+Set-Alias last Get-LastItems
+
+function Get-FullHistory {
+	Get-Content (Get-PSReadlineOption).HistorySavePath
+}
+
+Set-Alias history Get-FullHistory
