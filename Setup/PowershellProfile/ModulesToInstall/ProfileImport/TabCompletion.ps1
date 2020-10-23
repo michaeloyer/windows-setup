0649403c96a06fe6657ef38288f141ccc387e60f
Michael Oyer <michael.oyer@ravenind.com>
2020-10-23 18:07:28 -0400

    Use wild card search for ssh hostnames

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -2,10 +2,22 @@ Register-ArgumentCompleter -CommandName 'ssh' -ScriptBlock {
     param($commandName, $wordToComplete)
 
 	switch -regex ($wordToComplete) {
-		'^\s*ssh\s*$' {
+		'^\s*ssh\s*[*\w]*$' {
+			$match = [Regex]::Match($wordToComplete, 'ssh\s+([*\w]+)')
+			$hasSubcommand = $match.Success
+			$subcommand = $match.Groups[1].Value
+			
 			Get-Content ~/.ssh/config |
 			Select-String ^Host -Raw |
-			ForEach-Object { $_ -split '\s+' | Select -Skip 1 }
+			ForEach-Object { $_ -split '\s+' | Select -Skip 1 } |
+			Where-Object { 
+				if ($hasSubcommand) 
+					{ $_ -like "$subcommand*" } 
+				else 
+					{ $true }
+				
+			} |
+			Sort-Object 
 		}
 	}
 }
