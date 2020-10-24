126fc196183df1cc8c713c76c9c954d37f777b4f
Michael Oyer <code@michaeloyer.com>
2020-10-24 17:55:46 -0400

    Include known_hosts in ssh tab completion

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -1,23 +1,30 @@
 Register-ArgumentCompleter -CommandName 'ssh' -ScriptBlock {
     param($commandName, $wordToComplete)
-
-	switch -regex ($wordToComplete) {
-		'^\s*ssh\s*[*\w]*$' {
-			$match = [Regex]::Match($wordToComplete, 'ssh\s+([*\w]+)')
-			$hasSubcommand = $match.Success
-			$subcommand = $match.Groups[1].Value
-			
-			Get-Content ~/.ssh/config |
+	
+	function Get-Hosts() {	
+		$ssh_hosts = 
+			Get-Content ~/.ssh/config -ErrorAction Ignore |
 			Select-String ^Host -Raw |
-			ForEach-Object { $_ -split '\s+' | Select -Skip 1 } |
-			Where-Object { 
-				if ($hasSubcommand) 
-					{ $_ -like "$subcommand*" } 
-				else 
-					{ $true }
-				
-			} |
-			Sort-Object 
+			ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+					
+		$known_hosts = 
+			Get-Content ~/.ssh/known_hosts  -ErrorAction Ignore | 
+			ForEach-Object { $_ -split '\s' | Select-Object -First 1 } | 
+			ForEach-Object { $_ -split ',' } |
+			ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+			
+		($ssh_hosts + $known_hosts) | Select-Object -Unique
+	}
+	
+	switch -regex ($wordToComplete) {
+		'\s[\*\w\.]+$' {
+			$partialHost = $($wordToComplete -split ' ' | Select-Object -Last 1)
+			Get-Hosts | Where-Object { $_ -like "$partialHost*" }
+		}	
+		default { 
+			Get-Hosts 
 		}
 	}
 }
@@ -29,7 +36,7 @@ Register-ArgumentCompleter -CommandName 'npm' -ScriptBlock {
 		'^\s*npm\s+run\s*$' {
 			Get-Content .\package.json -Raw | 
 			ConvertFrom-Json -AsHashtable | 
-			Select -ExpandProperty scripts | 
+			Select-Object -ExpandProperty scripts | 
 			ForEach-Object { $_.Keys } |
 			Sort-Object 
 		}
