2ed0c643fc8a0bfa365dd56bd24b9f020d1e63ea
Michael Oyer <code@michaeloyer.com>
2020-10-24 22:36:15 -0400

    Improve Tab Completions

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -1,47 +1,44 @@
-Register-ArgumentCompleter -CommandName 'ssh' -ScriptBlock {
-    param($commandName, $wordToComplete)
+Register-ArgumentCompleter -Native -CommandName 'ssh' -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
 	
-	function Get-Hosts() {	
-		$ssh_hosts = 
-			Get-Content ~/.ssh/config -ErrorAction Ignore |
-			Select-String ^Host -Raw |
-			ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
-			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+	switch -Wildcard ($commandAst) {
+		'*-i' {
+			Get-ChildItem ~/.ssh | 
+			Select -ExpandProperty FullName |
+			ForEach-Object { "$wordToComplete $_".TrimStart() }
+		}
+		default {
+			$ssh_hosts = 
+				Get-Content ~/.ssh/config -ErrorAction Ignore |
+				Select-String ^Host -Raw |
+				ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
+				Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
 					
-		$known_hosts = 
-			Get-Content ~/.ssh/known_hosts  -ErrorAction Ignore | 
-			ForEach-Object { $_ -split '\s' | Select-Object -First 1 } | 
-			ForEach-Object { $_ -split ',' } |
-			ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
-			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
-			
-		($ssh_hosts + $known_hosts) | Select-Object -Unique
-	}
-	
-	switch -regex ($wordToComplete) {
-		'-i$' {
-			Get-ChildItem ~/.ssh | Select -ExpandProperty FullName
-		}
-		'\s[\*\w\.]+$' {
-			$partialHost = $($wordToComplete -split ' ' | Select-Object -Last 1)
-			Get-Hosts | Where-Object { $_ -like "$partialHost*" }
-		}
-		default { 
-			Get-Hosts 
+			$known_hosts =
+				Get-Content ~/.ssh/known_hosts -ErrorAction Ignore |
+				ForEach-Object { $_ -split '\s' | Select-Object -First 1 } |
+				ForEach-Object { $_ -split ',' } |
+				ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
+				Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+				
+			($ssh_hosts + $known_hosts) |
+			Select-Object -Unique |
+			Where-Object { $_ -like "$wordToComplete*" }
 		}
 	}
 }
 
-Register-ArgumentCompleter -CommandName 'npm' -ScriptBlock {
-    param($commandName, $wordToComplete)
+Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
 
-	switch -regex ($wordToComplete) {
-		'^\s*npm\s+run\s*$' {
-			Get-Content .\package.json -Raw | 
-			ConvertFrom-Json -AsHashtable | 
-			Select-Object -ExpandProperty scripts | 
+	switch -Regex ($commandAst) {
+		"^npm\s+run\b" {
+			Get-Content .\package.json -Raw -ErrorAction Ignore |
+			ConvertFrom-Json -AsHashtable -ErrorAction Ignore  |
+			Select-Object -ExpandProperty scripts |
 			ForEach-Object { $_.Keys } |
-			Sort-Object 
+			Where-Object { $_ -like "$wordToComplete*" } |
+			Sort-Object
 		}
 	}
 }
