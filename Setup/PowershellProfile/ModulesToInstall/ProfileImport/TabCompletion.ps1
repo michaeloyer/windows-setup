90664a6a44d426e6c6894b2899362a8a291d671d
Michael Oyer <code@michaeloyer.com>
2020-12-11 17:46:23 -0500

    Add more npm commands, add nvm

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -58,5 +58,41 @@ Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
 			Where-Object { $_ -like "$wordToComplete*" } |
 			Sort-Object
 		}
+		'' {
+			@('run', 'access', 'adduser', 'audit', 'bin', 'bugs', 'c', 'cache', 'ci', 'cit',
+				'clean-install', 'clean-install-test', 'completion', 'config',
+				'create', 'ddp', 'dedupe', 'deprecate', 'dist-tag', 'docs', 'doctor',
+				'edit', 'explore', 'get', 'help', 'help-search', 'hook', 'i', 'init',
+				'install', 'install-ci-test', 'install-test', 'it', 'link', 'list', 'ln',
+				'login', 'logout', 'ls', 'org', 'outdated', 'owner', 'pack', 'ping', 'prefix',
+				'profile', 'prune', 'publish', 'rb', 'rebuild', 'repo', 'restart', 'root',
+				'run-script', 's', 'se', 'search', 'set', 'shrinkwrap', 'star',
+				'stars', 'start', 'stop', 't', 'team', 'test', 'token', 'tst', 'un',
+				'uninstall', 'unpublish', 'unstar', 'up', 'update', 'v', 'version', 'view',
+				'whoami') |
+					Where-Object { $_ -like "$wordToComplete*" }
+		}
+	}
+}
+
+Register-ArgumentCompleter -Native -CommandName 'nvm' -ScriptBlock {
+	param($wordToComplete, $commandAst, $cursorPosition)
+	
+	$context = Get-TabCompleteContext $wordToComplete $commandAst
+	
+	switch ($context) {
+		'' {
+			return nvm --help | 
+				Select-String 'nvm' | 
+				ForEach-Object { ($_ -split '\s+', 4)[2] } |
+				Where-Object { $_ -like "$wordToComplete*" }
+		}
+		'use' {
+			return nvm list | 
+				Where-Object { $_ -match '\d+' -and $_ -notmatch '\*' } | 
+				ForEach-Object { ($_ -split '[\*\s]+')[1] } |
+				Where-Object { $_ -like "$wordToComplete*" }
+
+		}
 	}
 }
