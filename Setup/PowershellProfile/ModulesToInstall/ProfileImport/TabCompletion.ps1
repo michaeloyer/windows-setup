21f69a29e535472155081329b060014a57f9e0f2
Michael Oyer <code@michaeloyer.com>
2020-12-11 17:10:02 -0500

    Improve Powershell Tab Completion Switch Statements

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -1,26 +1,42 @@
+function Get-TabCompleteContext($wordToComplete, $commandAst) {
+	if ($commandAst -notmatch ' ') {
+		return ''
+	}
+
+	$subCommands = ($commandAst -split ' ', 2)[1].Trim() -replace '\s+', ' ' `
+		-replace '(\B|\s)\-.*(?=\s\-)', ""
+
+	$context = ($subCommands -replace ([Regex]::Escape($wordToComplete) + "$"), '').Trim() `
+		-replace '\s+' + [Regex]::Escape($wordToComplete) + "$", ''
+
+	return $context 
+}
+
 Register-ArgumentCompleter -Native -CommandName 'ssh' -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
+	param($wordToComplete, $commandAst, $cursorPosition)
+
+	$context = Get-TabCompleteContext $wordToComplete $commandAst
 	
-	switch -Wildcard ($commandAst) {
-		'*-i' {
+	switch ($context) {
+		'-i' {
 			Get-ChildItem ~/.ssh | 
 			Select -ExpandProperty FullName |
 			ForEach-Object { "$wordToComplete $_".TrimStart() }
 		}
-		default {
+		'' {
 			$ssh_hosts = 
-				Get-Content ~/.ssh/config -ErrorAction Ignore |
-				Select-String ^Host -Raw |
-				ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
-				Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
-					
+			Get-Content ~/.ssh/config -ErrorAction Ignore |
+			Select-String ^Host -Raw |
+			ForEach-Object { $_ -split '\s+' | Where-Object { -not [string]::IsNullOrEmpty($_) } | Select-Object -Skip 1 } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+			
 			$known_hosts =
-				Get-Content ~/.ssh/known_hosts -ErrorAction Ignore |
-				ForEach-Object { $_ -split '\s' | Select-Object -First 1 } |
-				ForEach-Object { $_ -split ',' } |
-				ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
-				Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
-				
+			Get-Content ~/.ssh/known_hosts -ErrorAction Ignore |
+			ForEach-Object { $_ -split '\s' | Select-Object -First 1 } |
+			ForEach-Object { $_ -split ',' } |
+			ForEach-Object { $_ -replace '^\[' -replace '\]:\d+$' } |
+			Sort-Object -Property @{Expression={[ipaddress]::TryParse($_, [ref]$null) }}, @{Expression={$_}}
+			
 			($ssh_hosts + $known_hosts) |
 			Select-Object -Unique |
 			Where-Object { $_ -like "$wordToComplete*" }
@@ -29,10 +45,12 @@ Register-ArgumentCompleter -Native -CommandName 'ssh' -ScriptBlock {
 }
 
 Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-
-	switch -Regex ($commandAst) {
-		"^npm\s+run\b" {
+	param($wordToComplete, $commandAst, $cursorPosition)
+	
+	$context = Get-TabCompleteContext $wordToComplete $commandAst
+	
+	switch ($context) {
+		'run' {
 			Get-Content .\package.json -Raw -ErrorAction Ignore |
 			ConvertFrom-Json -AsHashtable -ErrorAction Ignore  |
 			Select-Object -ExpandProperty scripts |
