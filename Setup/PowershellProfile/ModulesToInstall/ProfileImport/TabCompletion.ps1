038ffe5a89c72ba0fc1c76959b6679bf5284a56a
Michael Oyer <code@michaeloyer.com>
2022-04-17 21:37:39 -0400

    Update npm root commands

--- Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
+++ Setup/PowershellProfile/ModulesToInstall/ProfileImport/TabCompletion.ps1
@@ -82,17 +82,8 @@ Register-ArgumentCompleter -Native -CommandName 'npm' -ScriptBlock {
             Sort-Object
         }
         '' {
-            @('run', 'access', 'adduser', 'audit', 'bin', 'bugs', 'c', 'cache', 'ci', 'cit',
-                'clean-install', 'clean-install-test', 'completion', 'config',
-                'create', 'ddp', 'dedupe', 'deprecate', 'dist-tag', 'docs', 'doctor',
-                'edit', 'explore', 'get', 'help', 'help-search', 'hook', 'i', 'init',
-                'install', 'install-ci-test', 'install-test', 'it', 'link', 'list', 'ln',
-                'login', 'logout', 'ls', 'org', 'outdated', 'owner', 'pack', 'ping', 'prefix',
-                'profile', 'prune', 'publish', 'rb', 'rebuild', 'repo', 'restart', 'root',
-                'run-script', 's', 'se', 'search', 'set', 'shrinkwrap', 'star',
-                'stars', 'start', 'stop', 't', 'team', 'test', 'token', 'tst', 'un',
-                'uninstall', 'unpublish', 'unstar', 'up', 'update', 'v', 'version', 'view',
-                'whoami') |
+            @('run') +
+            [regex]::Match($(npm --help), "(?<=All commands:\s+)([\w\-]+,\s+)+[\w\-]+") -split ',\s+' |
             Where-Object { $_ -like "$wordToComplete*" }
         }
     }
