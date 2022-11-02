0f32cd3c0cd55430e765140bd0e0f739c3e53f77
Michael Oyer <code@michaeloyer.com>
2022-11-01 23:30:37 -0400

    No longer install Posh-Git Prerelease

--- Setup/Git/posh_git_install.ps1
+++ Setup/Git/posh_git_install.ps1
@@ -1,7 +1,7 @@
 Write-Host 'Installing ' -ForegroundColor Yellow -NoNewline
 Write-Host 'posh-git' -ForegroundColor Red
 
-PowerShellGet\Install-Module posh-git -Force -AllowPrerelease -Scope CurrentUser
+PowerShellGet\Install-Module posh-git -Force -Scope CurrentUser
 
 if ((Get-Module posh-git) -ne $null) {
     Write-Host 'Successfully installed ' -ForegroundColor Yellow -NoNewline
